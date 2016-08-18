bbbf75fe8217e20eb4c490eb83ea980394ea9079
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-18 15:26:44 -0500

    Update ejemplo-4.js

--- ejemplo-4.js
+++ ejemplo-4.js
@@ -1,7 +1,7 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
-var renderizador=new THREE.WebGLRenderer();
 camara.position.z=5;
+var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 var forma=new THREE.BoxGeometry(1,1,1);
@@ -9,3 +9,6 @@ var material=new THREE.MeshNormalMaterial();
 var cubo=new THREE.Mesh(forma,material);
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
+escena.add(cubo);
+renderizador.render(escena,camara);
+
