22a30098ad092e9e454f6334a770d257fab09284
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-22 02:03:42 -0500

    Update Ejercicio2_1.js

--- rv/Ejercicio2_1.js
+++ rv/Ejercicio2_1.js
@@ -4,7 +4,7 @@ camara.position.z=7;
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
-var forma=new THREE.CylinderGeometry(.2, .2, .2, .2);
+var forma=new THREE.CylinderGeometry(.2, .2, .2);
 var forma2=new THREE.SphereGeometry(1,8,6);
 var material=new THREE.MeshNormalMaterial();
 var cilindro=new THREE.Mesh(forma,material);
