0c2d206685e77a33704d0e9de27ca2d0372fa156
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-25 15:11:16 -0500

    Update Ejemplo6Geometry.js

--- Ejemplo6Geometry.js
+++ Ejemplo6Geometry.js
@@ -9,7 +9,7 @@ var escena=new THREE.Scene();
 escena.add(malla);
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=100;
-var.renderizador=new THREE.WebGLRenderer();
+var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
