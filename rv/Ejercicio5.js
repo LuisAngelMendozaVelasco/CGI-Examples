b83ca30e35c50ba56f3595a3124bd3d3a017d897
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-30 15:41:33 -0500

    Update Ejercicio5.js

--- rv/Ejercicio5.js
+++ rv/Ejercicio5.js
@@ -6,9 +6,9 @@ for (var i=30;i<50;i++){
                   }
 var forma=new THREE.LatheGeometry(puntos);
 
-for (var i=0;i<50;i++){
+for (var i=0;i<25;i++){
   puntos2.push(new THREE.Vector2(
-                  i+1,1+2));
+                  50,i+1));
                   }
 var forma2=new THREE.LatheGeometry(puntos2);
 
