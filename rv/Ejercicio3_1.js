d7b83b819be9780692c2c58129fb2ca1769e4302
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-30 23:02:40 -0500

    Update Ejercicio3_1.js

--- rv/Ejercicio3_1.js
+++ rv/Ejercicio3_1.js
@@ -14,14 +14,14 @@ forma.faces.push(new THREE.Face3(4,6,7)); //cara2
 forma.faces.push(new THREE.Face3(4,5,6)); //cara3
 
 forma.faces.push(new THREE.Face3(0,3,4)); //cara4
-forma.faces.push(new THREE.Face3(0,3,7)); //cara5
+forma.faces.push(new THREE.Face3(3,4,7)); //cara5
 forma.faces.push(new THREE.Face3(1,2,5)); //cara6
-forma.faces.push(new THREE.Face3(1,2,6)); //cara7
+forma.faces.push(new THREE.Face3(2,5,6)); //cara7
 
 forma.faces.push(new THREE.Face3(2,3,6)); //cara0
 forma.faces.push(new THREE.Face3(3,6,7)); //cara1
 forma.faces.push(new THREE.Face3(0,1,5)); //cara0
-forma.faces.push(new THREE.Face3(0,1,4)); //cara1
+forma.faces.push(new THREE.Face3(0,4,5)); //cara1
 
 
 forma.computeBoundingSphere();
