5eda7932eaccf1a09bb06052d50604f5bdfbfc76
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-08-30 22:32:12 -0500

    Update Ejercicio3_1.js

--- rv/Ejercicio3_1.js
+++ rv/Ejercicio3_1.js
@@ -1,20 +1,16 @@
 var forma=new THREE.Geometry();
-forma.vertices.push(new THREE.Vector3(1,0,1)); //vector0
-forma.vertices.push(new THREE.Vector3(1,0,-1)); //vector1
-forma.vertices.push(new THREE.Vector3(-1,0,-1)); //vector2
-forma.vertices.push(new THREE.Vector3(-1,0,1)); //vector3
+forma.vertices.push(new THREE.Vector3(1,-1,1)); //vector0
+forma.vertices.push(new THREE.Vector3(1,-1,-1)); //vector1
+forma.vertices.push(new THREE.Vector3(-1,-1,-1)); //vector2
+forma.vertices.push(new THREE.Vector3(-1,-1,1)); //vector3
 forma.vertices.push(new THREE.Vector3(1,1,1)); //vector4
 forma.vertices.push(new THREE.Vector3(1,1,-1)); //vector5
 forma.vertices.push(new THREE.Vector3(-1,1,-1)); //vector6
 forma.vertices.push(new THREE.Vector3(-1,1,1)); //vector7
-forma.faces.push(new THREE.Face3(0,1,2)); //cara0
-forma.faces.push(new THREE.Face3(0,2,3)); //cara1
 
-forma.faces.push(new THREE.Face3(0,1,5)); //cara2
-forma.faces.push(new THREE.Face3(0,4,5)); //cara3
+forma.faces.push(new THREE.Face3(0,2,3)); //cara0
+forma.faces.push(new THREE.Face3(0,1,2)); //cara1
 
-forma.faces.push(new THREE.Face3(4,5,6)); //cara2
-forma.faces.push(new THREE.Face3(4,6,7)); //cara3
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
