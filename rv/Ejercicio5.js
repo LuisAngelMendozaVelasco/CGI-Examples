b77d2dfc97e6574e44779385bc0847c262cd449b
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-02 00:30:53 -0500

    Update Ejercicio5.js

--- rv/Ejercicio5.js
+++ rv/Ejercicio5.js
@@ -13,9 +13,31 @@ var material=new THREE.MeshNormalMaterial();
 var malla= new THREE.Mesh(forma,material);
 //malla2.rotateX(Math.PI/6);
 
+var torre2=new THREE.Shape();
+
+torre2.moveTo(-30,90);
+torre2.lineTo(-30,30);
+torre2.lineTo(-90,30);
+torre2.lineTo(-90,-30);
+torre2.lineTo(-30,-30);
+torre2.lineTo(-30,-90);
+torre2.lineTo(30,-90);
+torre2.lineTo(30,-30);
+torre2.lineTo(90,-30);
+torre2.lineTo(90,30);
+torre2.lineTo(30,30);
+torre2.lineTo(30,90);
+torre2.lineTo(-30,90);
+
+var forma2= new THREE.ExtrudeGeometry(torre2,{amount:40});
+var material2=new THREE.MeshNormalMaterial();
+var malla2= new THREE.Mesh(forma2,material2);
+malla2.translate(0,140,0);
+
 
 var escena=new THREE.Scene();
 escena.add(malla);
+escena.add(malla2);
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
 var renderizador=new THREE.WebGLRenderer();
