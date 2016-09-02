2a858f93a9d4c731a13fc7d059a7c2067c827b1a
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-02 11:10:45 -0500

    Update Ejercicio5.js

--- rv/Ejercicio5.js
+++ rv/Ejercicio5.js
@@ -16,37 +16,29 @@ torre2.lineTo(-57.43,30);
 torre2.lineTo(-84.85,30);
 torre2.lineTo(-30,84.85);
 
-var torre2=new THREE.Shape();
-
-torre2.moveTo(-30,84.85);
-torre2.lineTo(-30,57.43);
-torre2.lineTo(-57.43,30);
-torre2.lineTo(-84.85,30);
-torre2.lineTo(-30,84.85);
-
 var torre2_1=new THREE.Shape();
 
-torre2_1.moveTo(-30,84.85);
-torre2_1.lineTo(-30,57.43);
-torre2_1.lineTo(-57.43,30);
-torre2_1.lineTo(-84.85,30);
-torre2_1.lineTo(-30,84.85);
+torre2_1.moveTo(-30,-84.85);
+torre2_1.lineTo(-30,-57.43);
+torre2_1.lineTo(-57.43,-30);
+torre2_1.lineTo(-84.85,-30);
+torre2_1.lineTo(-30,-84.85);
 
 var torre2_2=new THREE.Shape();
 
-torre2_2.moveTo(-30,84.85);
-torre2_2.lineTo(-30,57.43);
-torre2_2.lineTo(-57.43,30);
-torre2_2.lineTo(-84.85,30);
-torre2_2.lineTo(-30,84.85);
+torre2_2.moveTo(30,-84.85);
+torre2_2.lineTo(30,-57.43);
+torre2_2.lineTo(57.43,-30);
+torre2_2.lineTo(84.85,-30);
+torre2_2.lineTo(30,-84.85);
 
 var torre2_3=new THREE.Shape();
 
-torre2_3.moveTo(-30,84.85);
-torre2_3.lineTo(-30,57.43);
-torre2_3.lineTo(-57.43,30);
-torre2_3.lineTo(-84.85,30);
-torre2_3.lineTo(-30,84.85);
+torre2_3.moveTo(30,84.85);
+torre2_3.lineTo(30,57.43);
+torre2_3.lineTo(57.43,30);
+torre2_3.lineTo(84.85,30);
+torre2_3.lineTo(30,84.85);
 
 var torre3 = new THREE.CylinderGeometry(90,90,5,32);
 
