18e1956349aa67313ecd067ec1e1b36834d52fd5
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-06 15:01:47 -0500

    Update Ejemplo10.js

--- Ejemplo10.js
+++ Ejemplo10.js
@@ -20,7 +20,7 @@ escena.add(esfera1);
 escena.add(esfera2);
 escena.add(cubo);
 
-var rendrizador=new THREE.WebGLRenderer();
+var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(renderizador.domElement);
