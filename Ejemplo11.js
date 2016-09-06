dbf5051f029251e4dbfb7d408f76ef29d26d881e
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-06 15:25:56 -0500

    Update Ejemplo11.js

--- Ejemplo11.js
+++ Ejemplo11.js
@@ -25,5 +25,5 @@ escena.add(esfera2);
 escena.add(cubo);
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderizador.domElemenet);
+document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camera)
