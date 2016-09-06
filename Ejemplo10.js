6b07b47b03ec16460ee3f62ba255f43ab42ca50c
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-06 14:59:52 -0500

    Update Ejemplo10.js

--- Ejemplo10.js
+++ Ejemplo10.js
@@ -10,7 +10,7 @@ camara.position.z=15;
 var cubo=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
 var esfera1=new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial());
-esferal.position.x=5;
+esfera1.position.x=5;
 var esfera2=new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial());
 esfera2.position.x=-5;
 esefra2.position.z=-10;
