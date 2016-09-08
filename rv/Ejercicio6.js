06b0fe52eeff5dcddff1c1eea481f2b58fe9a9e6
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-09-08 14:55:23 -0500

    Update Ejercicio6.js

--- rv/Ejercicio6.js
+++ rv/Ejercicio6.js
@@ -17,7 +17,8 @@
 				var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
 
 				var line = new THREE.LineSegments( geometry, material );
-				scene.add( line );
+var scene=new THREE.Scene();
+scene.add( line );
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
 var renderizador=new THREE.WebGLRenderer();
