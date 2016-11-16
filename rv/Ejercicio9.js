354de41d0a72cad99eb0dc2834891ffc129097b7
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-11-15 23:27:28 -0600

    Update Ejercicio9.js

--- rv/Ejercicio9.js
+++ rv/Ejercicio9.js
@@ -10,7 +10,7 @@ var prototipo = new Object();
   camara.position.z=130;
   camara.position.x=45;
   camara.position.y=-45;
-  escena.rotateX(Math.PI*0.7)
+  escena.rotateX(Math.PI*0.5)
 
 prototipo.TorreGeometry= function() {
   THREE.Geometry.call(this);
