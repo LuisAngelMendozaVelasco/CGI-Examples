bece6f9ca779c5459e4d5a940d5fa22036609892
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-11-17 01:30:43 -0600

    Update Ejercicio10.js

--- rv/Ejercicio10.js
+++ rv/Ejercicio10.js
@@ -31,7 +31,7 @@ function init(){
     
 torre1=new THREE.Mesh(torreForma,marmolblanco);
     torre1.position.y=10;
-    torre1.position.z=-5;
+    torre1.position.z=-1;
     torre1.position.x=10;
     torre1.scale.set(0.05,0.05,0.05);
     torre1.rotateX(-Math.PI/2);
