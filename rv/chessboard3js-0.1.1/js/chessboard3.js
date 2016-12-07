503feb77a95102641c9bddd3f69c432580ecb32a
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-12-06 22:11:27 -0600

    Update chessboard3.js

--- rv/chessboard3js-0.1.1/js/chessboard3.js
+++ rv/chessboard3js-0.1.1/js/chessboard3.js
@@ -586,7 +586,7 @@
                 if (cfg.hasOwnProperty('backgroundColor') && typeof cfg.backgroundColor === 'number') {
                     backgroundColor = cfg.backgroundColor;
                 } else {
-                    backgroundColor = 0x00FFFF;
+                    backgroundColor = 0x0080C0;
                 }
                 RENDERER.setClearColor(backgroundColor, 1);
 
