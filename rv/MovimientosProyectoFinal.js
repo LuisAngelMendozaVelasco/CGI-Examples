0068c16d64d7eb41a410a0382c7a4dc3d5afe470
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-12-07 00:54:21 -0600

    Update MovimientosProyectoFinal.js

--- rv/MovimientosProyectoFinal.js
+++ rv/MovimientosProyectoFinal.js
@@ -130,7 +130,7 @@ function createBoard(pieceSet) {
                     cfg.lightSquareColor = 0x888888;
                     cfg.darkSquareColor = 0x666666;
                 }
-                cfg.pieceSet = 'Ajedrez/' + pieceSet + '/{piece}.json'
+                cfg.pieceSet = 'assets/chesspieces/' + pieceSet + '/{piece}.json'
             }
             return new ChessBoard3('board', cfg);
 
