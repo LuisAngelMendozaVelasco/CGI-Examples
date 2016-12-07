52138a46fb0fe3c68fb9d7ab3a83a51348f7d4aa
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-12-06 21:48:55 -0600

    Update chessboard3.js

--- rv/chessboard3js-0.1.1/js/chessboard3.js
+++ rv/chessboard3js-0.1.1/js/chessboard3.js
@@ -318,7 +318,7 @@
             }
             var darkSquareMaterial = new THREE.MeshPhongMaterial({color: new THREE.Color(darkSquareColor)});
 
-            var lightSquareColor= 0xf0d9b5;
+            var lightSquareColor= 0xFFFFFF;
             if (cfg.hasOwnProperty('lightSquareColor') && typeof cfg.lightSquareColor === 'number') {
                 lightSquareColor = cfg.lightSquareColor;
             }
