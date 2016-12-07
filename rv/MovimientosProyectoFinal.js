670c8ce46a7c365cd9a1013e9033f4beb50d4dde
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-12-07 00:23:34 -0600

    Update MovimientosProyectoFinal.js

--- rv/MovimientosProyectoFinal.js
+++ rv/MovimientosProyectoFinal.js
@@ -132,7 +132,7 @@ function createBoard(pieceSet) {
                 }
                 cfg.pieceSet = 'assets/chesspieces/' + pieceSet + '/{piece}.json';
             }
-            return new ChessBoard3('board', cfg);
+            return new Tablero('board', cfg);
 
     }
  windowsWidth = $(window).width()*0.75
