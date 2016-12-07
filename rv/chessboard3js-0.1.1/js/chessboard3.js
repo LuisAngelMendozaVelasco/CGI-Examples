d257b815ad83dfbd8cff922dba44a8f7e82718b5
LuisAngelMendozaVelasco <lmv_1712@live.com>
2016-12-06 21:37:27 -0600

    Update chessboard3.js

--- rv/chessboard3js-0.1.1/js/chessboard3.js
+++ rv/chessboard3js-0.1.1/js/chessboard3.js
@@ -276,7 +276,7 @@
             WHITE_MATERIAL.specular = new THREE.Color(whitePieceSpecular);
             WHITE_MATERIAL.transparent = true;
 
-            var blackPieceColor = 0x333333;
+            var blackPieceColor = 0x000000;
             if (cfg.hasOwnProperty('blackPieceColor') && typeof cfg.blackPieceColor === 'number') {
                 blackPieceColor = cfg.blackPieceColor;
             }
