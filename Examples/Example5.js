8b31f2ef08df55343459a8a2f268d48f2a64c26a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-08 20:38:12 -0600

    Create Example5.html

--- Examples/Example5.js
+++ Examples/Example5.js
@@ -1,11 +1,11 @@
-var forma = new THREE.BufferGeometry();
+var form = new THREE.BufferGeometry();
 var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh(forma, material);
-var escena  =new THREE.Scene();
-var camara = new THREE.PerspectiveCamera();
-var renderizador = new THREE.WebGLRenderer();
-
-const points = [new THREE.Vector3(-1, 1, -1),//c
+var mesh = new THREE.Mesh(form, material);
+var scene  =new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var renderer = new THREE.WebGLRenderer();
+const points = [
+                new THREE.Vector3(-1, 1, -1),//c
                 new THREE.Vector3(-1, -1, 1),//b
                 new THREE.Vector3(1, 1, 1),//a   
 
@@ -21,12 +21,13 @@ const points = [new THREE.Vector3(-1, 1, -1),//c
                 new THREE.Vector3(1, -1, -1),//d    
                 new THREE.Vector3(-1, -1, 1),//b
                 ];
-forma.setFromPoints(points);
-forma.computeVertexNormals();
-malla.rotateX(-Math.PI/4);
-malla.rotateY(Math.PI/4);
-escena.add(malla);
-camara.position.z = 5;
-renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
-document.body.appendChild(renderizador.domElement);
-renderizador.render(escena, camara);
+
+form.setFromPoints(points);
+form.computeVertexNormals();
+mesh.rotateX(-Math.PI/4);
+mesh.rotateY(Math.PI/4);
+scene.add(mesh);
+camera.position.z = 5;
+renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
