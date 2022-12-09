c230913551c6e38425e620fc8e9fc37ec6481422
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-08 20:17:31 -0600

    Create Example2.html

--- Examples/Example2.js
+++ Examples/Example2.js
@@ -2,4 +2,4 @@ var miCanvas = document.getElementById("Canvas1")
 var contexto = miCanvas.getContext("2d");
 
 contexto.fillStyle = "red";
-contexto.fillRect(30, 30, 50, 50);
+contexto.fillRect(25, 25, 50, 50);
