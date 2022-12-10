937399153a9dff2fa360a302c6d7e78db0176df3
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-10 12:55:16 -0600

    Update Example9.html

--- Examples/Example9.js
+++ Examples/Example9.js
@@ -1,29 +1,21 @@
-import {mergeBufferGeometries} from "../modules/BufferGeometryUtils.js";
+var trunk_form = new THREE.CylinderGeometry(.25, .5, 1);
+var sphere_form = new THREE.SphereGeometry(.65);
+sphere_form.translate(0, 1, 0);
 
-var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
-var esferaForma = new THREE.SphereGeometry(.65);
-esferaForma.translate(0, 1, 0);
-
-var troncoMalla = new THREE.Mesh(troncoForma);
-var esferaMalla = new THREE.Mesh(esferaForma);
-
-var arbolForma = mergeBufferGeometries([troncoMalla, esferaMalla]);
-
-// var arbolForma = new THREE.BufferGeometry();
-
-// arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
-// arbolForma.merge(esferaMalla.geometry,  esferaMalla.matrix);
+var trunk_mesh = new THREE.Mesh(trunk_form);
+var sphere_mesh = new THREE.Mesh(sphere_form);
+var tree_form = new THREE.Geometry();
+tree_form.merge(trunk_mesh.geometry, trunk_mesh.matrix);
+tree_form.merge(sphere_mesh.geometry,  sphere_mesh.matrix);
 
 var material = new THREE.MeshNormalMaterial();
-var arbolMalla = new THREE.Mesh(arbolForma, material);
+var tree_mesh = new THREE.Mesh(tree_form, material);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
 
-var escena = new THREE.Scene();
-escena.add(arbolMalla);
-
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-
-renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95, window.innerHeight*0.95);
-document.body.appendChild(renderizador.domElement);
-renderizador.render(escena, camara);
+scene.add(tree_mesh);
+camera.position.z = 5;
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95, window.innerHeight*0.95);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
