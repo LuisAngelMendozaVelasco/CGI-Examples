698b85f0f61527b3f0f63521e262150996a2a871
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-11 20:46:04 -0600

    Update Example19.html

--- Examples/Example19.js
+++ Examples/Example19.js
@@ -1,35 +1,32 @@
-var TEXTURA=new Object();
+var texture = new Object();
 
-TEXTURA.retrollamada=function(textura){
-  var material=new THREE.MeshBasicMaterial({map:textura});
-  TEXTURA.malla=new THREE.Mesh(new THREE.SphereGeometry(1),material);
-  TEXTURA.escena.add(TEXTURA.malla);
- }
- 
- TEXTURA.setup=function(){
-  TEXTURA.escena=new THREE.Scene();
-  
-  var cargador=new THREE.TextureLoader();
-  cargador.load('./earth_atmos_2048.jpg',TEXTURA.retrollamada);
-  
-  TEXTURA.camara=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-  
-  TEXTURA.camara.position.z=5;
-  
-  var lienzo=document.getElementById("ejemplo_textura");
-  TEXTURA.renderizador=new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
-  TEXTURA.renderizador.setSize(600,600);
- }
- 
- TEXTURA.loop=function(){
-  requestAnimationFrame(TEXTURA.loop);
-  
-  if(TEXTURA.malla!==undefined){
-    TEXTURA.malla.rotateX(0.01);
-    TEXTURA.malla.rotateY(0.01);
-  }
-  TEXTURA.renderizador.render(TEXTURA.escena,TEXTURA.camara);
- }
- 
- TEXTURA.setup();
- TEXTURA.loop();
+texture.callback = function(texture){
+    var material = new THREE.MeshBasicMaterial({map:texture});
+
+    texture.mesh = new THREE.Mesh(new THREE.SphereGeometry(3), material);
+    texture.scene.add(texture.mesh);
+}
+
+texture.setup = function(){
+    var loader = new THREE.TextureLoader();
+    var canvas = document.getElementById("texture_example");
+
+    loader.load('../images/earth_atmos_2048.jpg',  texture.callback);
+    texture.scene = new THREE.Scene();
+    texture.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    texture.camera.position.z = 5;
+    texture.renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true});
+texture.renderer.setSize(600, 600);
+}
+
+texture.loop = function(){
+    requestAnimationFrame(texture.loop);
+    if(texture.mesh !== undefined){
+        texture.mesh.rotateX(0.01);
+        texture.mesh.rotateY(0.01);
+    }
+    texture.renderer.render(texture.scene, texture.camera);
+}
+
+texture.setup();
+texture.loop();
