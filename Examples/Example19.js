0e1770d79da849cb9de0f7ec536263da6a10cb99
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-11 21:01:40 -0600

    Update Example19.js

--- Examples/Example19.js
+++ Examples/Example19.js
@@ -1,32 +1,32 @@
-var texture = new Object();
+var texture_object = new Object();
 
-texture.callback = function(texture_){
-    var material = new THREE.MeshBasicMaterial({map:texture_});
+texture_object.callback = function(texture){
+    var material = new THREE.MeshBasicMaterial({map:texture});
 
-    texture.mesh = new THREE.Mesh(new THREE.SphereGeometry(3), material);
-    texture.scene.add(texture.mesh);
+    texture_object.mesh = new THREE.Mesh(new THREE.SphereGeometry(3), material);
+    texture_object.scene.add(texture_object.mesh);
 }
 
-texture.setup = function(){
-    var loader = new THREE.TextureLoader();
+texture_object.setup = function(){
+    var loader = new THREE.Texture_objectLoader();
     var canvas = document.getElementById("texture_example");
 
-    loader.load('../images/earth_atmos_2048.jpg', texture.callback);
-    texture.scene = new THREE.Scene();
-    texture.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    texture.camera.position.z = 5;
-    texture.renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true});
-texture.renderer.setSize(600, 600);
+    loader.load('../images/earth_atmos_2048.jpg', texture_object.callback);
+    texture_object.scene = new THREE.Scene();
+    texture_object.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    texture_object.camera.position.z = 5;
+    texture_object.renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true});
+texture_object.renderer.setSize(600, 600);
 }
 
-texture.loop = function(){
-    requestAnimationFrame(texture.loop);
-    if(texture.mesh !== undefined){
-        texture.mesh.rotateX(0.01);
-        texture.mesh.rotateY(0.01);
+texture_object.loop = function(){
+    requestAnimationFrame(texture_object.loop);
+    if(texture_object.mesh !== undefined){
+        texture_object.mesh.rotateX(0.01);
+        texture_object.mesh.rotateY(0.01);
     }
-    texture.renderer.render(texture.scene, texture.camera);
+    texture_object.renderer.render(texture_object.scene, texture_object.camera);
 }
 
-texture.setup();
-texture.loop();
+texture_object.setup();
+texture_object.loop();
