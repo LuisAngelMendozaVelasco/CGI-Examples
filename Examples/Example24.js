72eb871c67d374348a9fbcf889f0a681ccdadd37
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-12 21:13:01 -0600

    Update Example24.js

--- Examples/Example24.js
+++ Examples/Example24.js
@@ -32,7 +32,7 @@ Environment.prototype.sense = function(){
 }
 
 Environment.prototype.plan = function(){
-    for (var i=0; i<this.children.length; i++){
+    for(var i=0; i<this.children.length; i++){
         if(this.children[i].plan !== undefined)
             this.children[i].plan(this)
     }
@@ -124,7 +124,7 @@ Environment.prototype.setMap = function(map){
     var _offset = Math.floor(map.length/2); 
     for(var i = 0; i<map.length; i++){
         for(var j = 0; j<map.length; j++){
-            if (map[i][j] ===   "x")
+            if (map[i][j] === "x")
                 this.add(new Wall(1, j-_offset, -(i-_offset))); 
             else if (map[i][j] === "r")
                 this.add(new Robot(0.5, j-_offset, -(i-_offset))); 
@@ -134,6 +134,7 @@ Environment.prototype.setMap = function(map){
 
 function setup(){
     var mapa = new Array(); 
+
     mapa[0] =   "xxxxxxxxxxxxxxxxxxxxxxxxx"; 
     mapa[1] =   "xr               r      x"; 
     mapa[2] =   "x                       x"; 
@@ -162,13 +163,8 @@ function setup(){
 
     environment = new Environment(); 
     environment.setMap(mapa); 
-
     camera = new THREE.PerspectiveCamera(); 
     camera.position.z = 30; 
-
-    renderer = new THREE.PerspectiveCamera(); 
-    camera.position.z = 30; 
-
     renderer = new THREE.WebGLRenderer(); 
     renderer.setSize(window.innerHeight*.95, window.innerHeight*.95)
     document.body.appendChild(renderer.domElement); 
@@ -180,11 +176,9 @@ function loop(){
     environment.sense(); 
     environment.plan(); 
     environment.act(); 
-    
     renderer.render(environment, camera); 
 }
 
 var environment, camera, renderer; 
-
 setup(); 
 loop(); 
