37d98ca25cac1d41d6d72bfc3b85b1d98716a5c7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2023-01-05 22:36:16 -0600

    Update Exercise1_3.js

--- Exercises/Exercise1_3.js
+++ Exercises/Exercise1_3.js
@@ -1,5 +1,5 @@
 var Canvas3 = document.getElementById("Canvas3")
-var contexto3 = Canvas3.getContext("2d");
+var context3 = Canvas3.getContext("2d");
 
-contexto3.fillStyle = "green";
-contexto3.fillRect(90, 60, 40, 20);
+context3.fillStyle = "green";
+context3.fillRect(90, 60, 40, 20);
